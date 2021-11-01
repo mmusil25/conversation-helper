834ba19b829c7e7046797eabc3bd6f8bf157d3ac
Mark Musil <musilmark25@gmail.com>
2021-11-01 15:14:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,54 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from transformers import pipeline
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+
 from kivy.properties import StringProperty
 from kivy.lang import Builder
 sent_analysis = pipeline("sentiment-analysis")
 
+class ScrollableLabel(ScrollView):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # ScrollView does not allow us to add more than one widget, so we need to trick it
+        # by creating a layout and placing two widgets inside it
+        # Layout is going to have one collumn and and size_hint_y set to None,
+        # so height wo't default to any size (we are going to set it on our own)
+        self.layout = GridLayout(cols=1, size_hint_y=None)
+        self.add_widget(self.layout)
+
+        # Now we need two wodgets - Label for chat history and 'artificial' widget below
+        # so we can scroll to it every new message and keep new messages visible
+        # We want to enable markup, so we can set colors for example
+        self.chat_history = Label(size_hint_y=None, markup=True)
+        self.scroll_to_point = Label()
+
+        # We add them to our layout
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_to_point)
+
+    # Methos called externally to add new message to the chat history
+    def update_chat_history(self, message):
+
+        # First add new line and message itself
+        self.chat_history.text += '\n' + message
+
+        # Set layout height to whatever height of chat history text is + 15 pixels
+        # (adds a bit of space at teh bottom)
+        # Set chat history label to whatever height of chat history text is
+        # Set width of chat history text to 98 of the label width (adds small margins)
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
+
+        # As we are updating above, text height, so also label and layout height are going to be bigger
+        # than the area we have for this widget. ScrollView is going to add a scroll, but won't
+        # scroll to the botton, nor there is a method that can do that.
+        # That's why we want additional, empty wodget below whole text - just to be able to scroll to it,
+        # so scroll to the bottom of the layout
+        self.scroll_to(self.scroll_to_point)
 
 
 class LoginScreen(BoxLayout):
@@ -49,7 +93,7 @@ class LoginScreen(BoxLayout):
         self.add_widget(self.lbl)
         #self.add_widget(Label(text='Message>', size_hint=(.5, .5),
          #              pos_hint={'center_x': .5, 'center_y': .1}))
-        self.interlocuteur = TextInput(multiline=True, font_size = 13, size_hint=(1, .4))
+        self.interlocuteur = TextInput(multiline=True, font_size = 13, size_hint=(1, .1))
         self.add_widget(self.interlocuteur)
         self.btn = Button(text="Enter", background_color = green, font_size = 13, size_hint=(.3, .1))
         self.add_widget(self.btn)
